aaae6690fb771ba96b3c1fb5ad6783e1f5426a5f
Md Sakibul Islam <86571674+Md-Sakibul-Islam@users.noreply.github.com>
2021-08-21 11:43:58 +0600

    final mac price

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// function to get total price
 function getTotalPrice() {
     const bestPrice = document.getElementById('bestPrice').innerText;
     const bestPriceNumber = parseInt(bestPrice);
@@ -15,71 +16,73 @@ function getTotalPrice() {
     return total;
 }
 
-function setPrice(totalAmount){
+// function for set total amount
+function setPrice(totalAmount) {
     const totalPrice = document.getElementById('total-price');
     totalPrice.innerText = totalAmount;
-  
+
     const total = document.getElementById('total');
-    total.innerText = totalAmount; 
+    total.innerText = totalAmount;
 }
 
-
-function setCost(ProductId,price){
-    const memoryPrice = document.getElementById(ProductId);
-    memoryPrice.innerText = price;
+//function for set product cost 
+function setCost(ProductId, price) {
+    const productCost = document.getElementById(ProductId);
+    productCost.innerText = price;
 }
 
-document.getElementById('bntEight').addEventListener('click',function(){
-    
-    setCost('memory',0);
-    const total =  getTotalPrice();
+// 8GB button click handler 
+document.getElementById('bntEight').addEventListener('click', function () {
+
+    setCost('memory', 0);
+    const total = getTotalPrice();
+    setPrice(total);
+});
+// 16GB button click handler 
+document.getElementById('btnSixteen').addEventListener('click', function () {
+    setCost('memory', 180);
+    const total = getTotalPrice();
+    setPrice(total);
+});
+// 256GB storage button click handler 
+document.getElementById('btnStorageOne').addEventListener('click', function () {
+    setCost('storage', 0);
+    const total = getTotalPrice();
     setPrice(total);
 });
 
-document.getElementById('btnSixteen').addEventListener('click',function(){
-    setCost('memory',180);
-    const total =  getTotalPrice();
+// 512GB storage button click handler 
+document.getElementById('btnStorageTwo').addEventListener('click', function () {
+    setCost('storage', 100);
+    const total = getTotalPrice();
+    setPrice(total);
+});
+// 1TB storage button click handler 
+document.getElementById('btnStorageThree').addEventListener('click', function () {
+    setCost('storage', 180);
+    const total = getTotalPrice();
+    setPrice(total);
+});
+// Free delivery button click handler
+document.getElementById('btnFreeCharge').addEventListener('click', function () {
+    setCost('deliveryCost', 0);
+    const total = getTotalPrice();
+    setPrice(total);
+});
+//  delivery charge button click handler
+document.getElementById('btnCharge').addEventListener('click', function () {
+    setCost('deliveryCost', 20);
+    const total = getTotalPrice();
     setPrice(total);
 });
 
-document.getElementById('btnStorageOne').addEventListener('click',function(){
-    setCost('storage',0);
-    const total =  getTotalPrice();
-    setPrice(total);
-});
-document.getElementById('btnStorageTwo').addEventListener('click',function(){
-    setCost('storage',100);
-    const total =  getTotalPrice();
-  setPrice(total);
-});
-document.getElementById('btnStorageThree').addEventListener('click',function(){
-    setCost('storage',180);
-    const total =  getTotalPrice();
-  setPrice(total);
-});
-
-document.getElementById('btnFreeCharge').addEventListener('click',function(){
-    setCost('deliveryCost',0);
-    const total =  getTotalPrice();
-    setPrice(total);
-});
-document.getElementById('btnCharge').addEventListener('click',function(){
-    setCost('deliveryCost',20);
-    const total =  getTotalPrice();
-    setPrice(total);
-});
-document.getElementById('apply').addEventListener('click',function(){
-    const promoCode =  document.getElementById('promo-code').value;
+// apply button click handler 
+document.getElementById('apply').addEventListener('click', function () {
+    const promoCode = document.getElementById('promo-code').value;
     const totalAmount = document.getElementById('total');
-  
-  
-  
-    if(promoCode == 'stevekaku'){
-       
-    const total = getTotalPrice() * 0.8;
-    totalAmount.innerText = total;
-        
-        
+    if (promoCode == 'stevekaku') {
+        const total = getTotalPrice() * 0.8;
+        totalAmount.innerText = total;
     }
-    document.getElementById('promo-code').value='';
-  });
+    document.getElementById('promo-code').value = '';
+});
